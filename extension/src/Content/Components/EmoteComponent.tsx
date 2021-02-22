6c2e034e1fe0a0b60b64dcac762dcdf2061ae403
Anatole <me@anatole.Dev>
2021-02-22 05:38:24 +0100

    Extension (EmoteComponent) - Hover tooltip

--- extension/src/Content/Components/EmoteComponent.tsx
+++ extension/src/Content/Components/EmoteComponent.tsx
@@ -1,4 +1,5 @@
 import Tooltip from '@material-ui/core/Tooltip';
+import { DataStructure } from '@typings/DataStructure';
 import * as React from 'react';
 import styled from 'styled-components';
 
@@ -14,7 +15,18 @@ export class Emote extends React.Component<Emote.Props, Emote.State> {
 	render() {
 		return (
 			<div style={{ display: 'inline-block' }}>
-				<Tooltip title={<h1 style={{ fontSize: '2.5em' }}>{this.props.name + ' (7TV Emote)'}</h1>} arrow>
+				<Tooltip title={
+					<React.Fragment>
+						<Emote.TooltipImage>
+							<img src={this.props.url}></img>
+						</Emote.TooltipImage>
+
+						<Emote.Details>
+							<h3 className='emote-name'> {this.props.emote.name} </h3>
+							<span className='emote-submitter'> by {this.props.emote.submitted_by} </span>
+						</Emote.Details>
+					</React.Fragment>
+				} arrow interactive>
 					<Emote.Style
 						className='seventv-emote'
 						onClick={(ev: React.MouseEvent) => this.openDetails(ev)}
@@ -41,6 +53,7 @@ export namespace Emote {
 	export interface Props {
 		name: string;
 		url: string;
+		emote: DataStructure.Emote;
 	}
 
 	export interface State {
@@ -51,10 +64,39 @@ export namespace Emote {
 		};
 	}
 
-	export const Wrapper = styled.div`
-
-	`;
 	export const Style = styled.div`
 		display: 'inline-flex';
+	`;
+
+	export const TooltipImage = styled.div`
+		margin: 1em;
+		height: 16em;
+		width: 16em;
+
+		display: flex;
+		justify-content: center;
+		img {
+			border: 1px solid ##dcdcdc
+			border-radius: 4px;
+			padding: 5px;
+			width: 16em;
+		}
+	`;
+
+	export const Details = styled.div`
+		display: block;
+
+		.emote-name {
+			width: 100%;
+			margin-bottom: 6px;
+		}
+
+		.emote-submitter {
+			font-size: 2em;
+		}
+
+		.is-7tv-emote {
+			font-size: 1.6em;
+		}
 	`;
 }
