67514c2d098d5128974a13161b67a13c69cac758
Anatole <me@anatole.Dev>
2021-03-30 07:53:53 +0200

    Styles/Sass

--- webpack.config.js
+++ webpack.config.js
@@ -4,80 +4,71 @@ const CopyPlugin = require('copy-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 const config = {
-  mode: 'development',
-  devtool: 'cheap-module-source-map',
-  entry: {
-    content: path.join(__dirname, 'src/Content/Content.tsx'),
-    background: path.join(__dirname, 'src/Background/Background.tsx'),
-    page: path.join(__dirname, 'src/Page/Page.tsx')
-  },
-  output: { path: path.join(__dirname, 'dist'), filename: '[name].js' },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-        exclude: /\.module\.css$/,
-      },
-      {
-        test: /\.ts(x)?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-              modules: true,
-            },
-          },
-        ],
-        include: /\.module\.css$/,
-      },
-      {
-        test: /\.svg$/,
-        use: 'file-loader',
-      },
-      {
-        test: /\.png$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              mimetype: 'image/png',
-            },
-          },
-        ],
-      },
-    ]
-  },
-  resolve: {
-    plugins: [new TsconfigPathsPlugin()],
-    extensions: ['.js', '.jsx', '.tsx', '.ts'],
-    alias: {
-      'react-dom': '@hot-loader/react-dom',
-    },
-  },
-  devServer: {
-    contentBase: './dist',
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [{ from: 'public', to: '.' }],
-    }),
-    new webpack.DefinePlugin({
-      AppMeta: JSON.stringify({ version: 1 })
-    }),
-  ],
+	mode: 'development',
+	devtool: 'cheap-module-source-map',
+	entry: {
+		content: path.join(__dirname, 'src/Content/Content.tsx'),
+		background: path.join(__dirname, 'src/Background/Background.tsx'),
+		page: path.join(__dirname, 'src/Page/Page.tsx')
+	},
+	output: { path: path.join(__dirname, 'dist'), filename: '[name].js' },
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				use: 'babel-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.ts(x)?$/,
+				loader: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: { outputPath: 'styles/', name: '[name].css'}
+                    },
+                    'sass-loader'
+                ]
+			},
+			{
+				test: /\.svg$/,
+				use: 'file-loader',
+			},
+			{
+				test: /\.png$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							mimetype: 'image/png',
+						},
+					},
+				],
+			},
+		]
+	},
+	resolve: {
+		plugins: [new TsconfigPathsPlugin()],
+		extensions: ['.js', '.jsx', '.tsx', '.ts'],
+		alias: {
+			'react-dom': '@hot-loader/react-dom',
+		},
+	},
+	devServer: {
+		contentBase: './dist',
+	},
+	plugins: [
+		new CopyPlugin({
+			patterns: [{ from: 'public', to: '.' }],
+		}),
+		new webpack.DefinePlugin({
+			AppMeta: JSON.stringify({ version: 1 })
+		}),
+	],
 };
 
 module.exports = config;
