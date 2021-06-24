b5afa638f9ff4cc3adfd9c36e17c29bad7509e3b
Anatole <me@anatole.Dev>
2021-06-24 18:27:05 +0200

    Add ffz_addon, ffz_loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,10 @@ const config = {
 	entry: {
 		content: path.join(__dirname, 'src/Content/Content.tsx'),
 		background: path.join(__dirname, 'src/Background/Background.tsx'),
-		page: path.join(__dirname, 'src/Page/Page.tsx')
+		page: path.join(__dirname, 'src/Page/Page.tsx'),
+
+		ffz_addon: path.join(__dirname, 'src/Page/FFZ/FrankerFaceZ.Addon.ts'),
+		ffz_loader: path.join(__dirname, 'src/Page/FFZ/FrankerFaceZ.Loader.ts'),
 	},
 	output: { path: path.join(__dirname, 'dist'), filename: '[name].js' },
 	module: {
