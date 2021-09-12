9e760d1269bc3a24e86d978c425a6b0d6ae781d8
Anatole <me@anatole.Dev>
2021-09-12 05:15:20 +0200

    Fix broken images in emote list when FFZ is on

--- src/Page/Page.tsx
+++ src/Page/Page.tsx
@@ -73,14 +73,17 @@ export class PageScript {
 		setInterval(async () => {
 			let channelName = this.getCurrentChannelFromURL();
 			if (channelName !== this.currentChannel) {
+				this.currentChannel = channelName;
 				Logger.Get().info(`Changing channel from ${this.currentChannel} to ${channelName}`);
 				controller = await this.awaitChatController();
 				this.eIndex = null;
 				this.currentChannelSet = null;
-				this.currentChannel = channelName;
 				channelName = controller.props.channelLogin;
 				channelID = controller.props.channelID;
-				switched(channelID, channelName, controller.props.userID ?? '');
+
+				if (channelName && channelID) {
+					switched(channelID, channelName, controller.props.userID ?? '');
+				}
 			}
 		}, 500);
 
@@ -91,7 +94,7 @@ export class PageScript {
 		return new Promise(resolve => {
 			const i = setInterval(() => {
 				const c = this.twitch.getChatController();
-				if (c && c.props?.channelID && c.props?.channelLogin) {
+				if (c && (c.props?.channelID && c.props?.channelLogin)) {
 					clearInterval(i);
 					resolve(c);
 				}
