c7f44eed1c4d6c7c2022cd1e72b868aed8df5cb7
Anatole <me@anatole.Dev>
2021-09-17 00:40:20 +0200

    Design youtube upgrade page

--- webpack.config.js
+++ webpack.config.js
@@ -71,6 +71,7 @@ module.exports = env => {
 				patterns: [
 					{ from: 'public/icon', to: '.', },
 					{ from: 'public/image', to: 'image/' },
+					{ from: 'public/yt_upgrade', to: 'yt_upgrade/' },
 					{
 						from: `public/manifest.v${env.MANIFEST_VERSION === '3' ? '3' : '2'}.json`,
 						to: 'manifest.json'
