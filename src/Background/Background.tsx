eb8597fd522ad411ab03bac02d95775994975137
Anatole <me@anatole.Dev>
2021-09-17 00:41:17 +0200

    Background: run popup after install

--- src/Background/Background.tsx
+++ src/Background/Background.tsx
@@ -1,3 +1,25 @@
 chrome.runtime.onInstalled.addListener(() => {
+	chrome.permissions.contains({
+		origins: ['*://*.youtube.com/*']
+	}, granted => {
+		if (granted) {
+			return undefined;
+		}
 
+		chrome.storage.local.get(items => {
+			if (items.yt_permissions_requested) {
+				return undefined;
+			}
+
+			chrome.windows.create({
+				url: 'yt_upgrade/yt_upgrade.html',
+				width: 800,
+				height: 700,
+				focused: true,
+				type: 'popup'
+			}, () => {
+				chrome.storage.local.set({ yt_permissions_requested: true });
+			});
+		});
+	});
 });
