c78b0746ed7ee3085c9ff451b851a7191a0498d7
Excellify <49245223+Excellify@users.noreply.github.com>
2021-09-20 17:24:47 +0200

    Changed " to '

--- src/Sites/app/EmoteMenu/EmoteMenuButton.tsx
+++ src/Sites/app/EmoteMenu/EmoteMenuButton.tsx
@@ -18,8 +18,8 @@ export class EmoteMenuButton extends React.Component<EmoteMenuButton.Props> {
 				<button ref={this.ref} onClick={ev => this.onClick(ev)}>
 					<img height={24} src={assetStore.get('7tv-nd.webp')} />
 				</button>
-				<span className={this.props.toSettings? "tooltip-under" : "tooltip-over"}>
-					{this.props.toSettings? "7TV Settings" : "7TV Emotes"}
+				<span className={this.props.toSettings? 'tooltip-under' : 'tooltip-over'}>
+					{this.props.toSettings? '7TV Settings' : '7TV Emotes'}
 				</span>
 			</div>
 		);
