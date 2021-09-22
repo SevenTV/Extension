2648091c367c55a36766bc4855432fac3384a1c8
gempir <gempir.dev@gmail.com>
2021-09-22 18:01:06 +0200

    rewrite emote menu button as function component

--- src/Sites/app/EmoteMenu/EmoteMenuButton.tsx
+++ src/Sites/app/EmoteMenu/EmoteMenuButton.tsx
@@ -1,42 +1,31 @@
 
 
-import React from 'react';
+import React, { useRef, MouseEvent } from 'react';
 import { MainComponent } from 'src/Sites/app/MainComponent';
 import { Logger } from 'src/Logger';
 import { assetStore } from 'src/Sites/app/SiteApp';
 
-export class EmoteMenuButton extends React.Component<EmoteMenuButton.Props> {
-	ref: React.RefObject<HTMLButtonElement>;
+export function EmoteMenuButton({ main, toSettings }: EmoteMenuButton.Props): JSX.Element {
+	const buttonRef = useRef<HTMLButtonElement>(null);
 
-	constructor(props: EmoteMenuButton.Props) {
-		super(props);
-
-		this.ref = React.createRef();
-	}
-
-	render() {
-		return (
-			<button ref={this.ref} title='7TV' onClick={ev => this.onClick(ev)} style={{ color: 'white' }}>
-				<img height={24} src={assetStore.get('7tv-nd.webp')} />
-			</button>
-		);
-	}
-
-	/**
-	 * Called when the user clicks the button
-	 */
-	private onClick(ev: React.MouseEvent): void {
-		ev.stopPropagation();
-		if (this.props.toSettings) {
+	const onClick = (e: MouseEvent): void => {
+		e.stopPropagation();
+		if (toSettings) {
 			Logger.Get().debug('EmoteMenuButton, action=onClick, to settings');
-			this.props.main?.openSettings();
+			main?.openSettings();
 			return undefined;
 		}
 
-		const bounds = this.ref.current?.getBoundingClientRect();
+		const bounds = buttonRef.current?.getBoundingClientRect();
 		Logger.Get().debug(`EmoteMenuButton, action=onClick, bounds=(x: ${bounds?.x}, y: ${bounds?.y})`);
-		this.props.main?.toggleEmoteMenu(bounds);
-	}
+		main?.toggleEmoteMenu(bounds);
+	};
+
+	return (
+		<button ref={buttonRef} title='7TV' onClick={onClick} style={{ color: 'white' }}>
+			<img height={24} src={assetStore.get('7tv-nd.webp')} />
+		</button>
+	);
 }
 
 export namespace EmoteMenuButton {
