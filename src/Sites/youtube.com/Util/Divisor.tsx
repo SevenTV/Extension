675559ba7c52c708287bbfd1b929887d0683be83
Excellify <jarlsgu@gmail.com>
2021-10-10 03:24:29 +0200

    lint v2

--- src/Sites/youtube.com/Util/Divisor.tsx
+++ src/Sites/youtube.com/Util/Divisor.tsx
@@ -5,23 +5,23 @@ export function Divisor(): JSX.Element {
 	const [clicked, setClick] = useState(false);
 
 	const divisor = document.querySelector('.seventv-divisor') as HTMLElement;
-  	const chatbox = document.querySelector('div[id=secondary]') as HTMLElement;
+	const chatbox = document.querySelector('div[id=secondary]') as HTMLElement;
 
 	const move = (e: any) => {
 		if ( e.pageX > 0 && e.pageX < e.view.innerWidth) {
-    		if (clicked) {
-    			divisor!.style.left = `${e.pageX}px`;
-        		chatbox!.style.width = `calc(100vw - ${e.pageX}px)`;
-    		}
+			if (clicked) {
+				divisor!.style.left = `${e.pageX}px`;
+				chatbox!.style.width = `calc(100vw - ${e.pageX}px)`;
+			}
 		}
 	};
 
 	return (
 		<div id='outer' onPointerMove={move}>
-    		<div
+			<div
 				id='inner'
-        		onPointerDown={() => {setClick(true)}}
-        		onPointerUp={() => {setClick(false)}}
+				onPointerDown={() => {setClick(true);}}
+				onPointerUp={() => {setClick(false);}}
 			/>
 		</div>
 	);
