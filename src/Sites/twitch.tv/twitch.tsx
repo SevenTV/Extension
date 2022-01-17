SevenTV/Extension

commit 2bc652e513a455ca639e5be2a417c19e6e417e2e

Enabled 7TV for embedded Twitch Chats (#193)
* Enabled 7TV for embedded Twitch Chats

Enabled 7TV for embedded Twitch Chats

Added comments

* suggested changes

* Lint

* Enabled 7TV for embedded Twitch Chats

Enabled 7TV for embedded Twitch Chats

Added comments

fixed trailing whitespace

updated staging manfiest

* Fixed changes

* Added 7tv changelog entry for 2.1.2

Co-authored-by: Ashwin K <ashwin.kuppahally@gmail.com>
Co-authored-by: Jarl Skajaa Gunnarsli <jarlsgu@gmail.com>

src/Sites/twitch.tv/twitch.tsx
@@ -41,6 +41,8 @@ export class TwitchPageScript {
 	 * cause major memory leak problems.
 	 */
 	constructor() {
+		if (!window.location.href.match(this.channelRegex)) return;
+
 		this.handleChannelSwitch();
 		this.avatarManager.check();
 

commit eedbbe510bd79fdbf62d764de5170bb3a07ca5ac

Remove breaking change introduced by #193

src/Sites/twitch.tv/twitch.tsx
@@ -34,17 +34,7 @@ export class TwitchPageScript {
 		return twitch;
 	}
 
-	/**
-	 * The PageScript is the lower layer of the extension, it nests itself directly into the page
-	 * in order to gain access to Twitch's react instance and components.
-	 *
-	 * The purpose of PageScript is primarily to relay info and events back to the content script,
-	 * no rendering should be done at this layer as it may conflict with Twitch itself, and can easily
-	 * cause major memory leak problems.
-	 */
 	constructor() {
-		if (!window.location.href.match(this.channelRegex)) return;
-
 		this.handleChannelSwitch();
 		this.avatarManager.check();
 