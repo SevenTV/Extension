a5cecc2a45ce7449c06c1fdfae79620031f859ef
Anatole <me@anatole.dev>
2023-04-07 13:47:08 +0200

    hosted manifest, local/hosted mode check

--- src/site/site.ts
+++ src/site/site.ts
@@ -1,15 +1,25 @@
+import { log } from "@/common/Logger";
+import { semanticVersionToNumber } from "@/common/Transform";
+
 (async () => {
-	const hostedURL = `http://127.0.0.1:8080/index.3.0.3.js`;
-	if (hostedURL) {
+	const manifestURL = `${import.meta.env.VITE_APP_HOST}/manifest.json`;
+	const manifest = await fetch(manifestURL)
+		.then((res) => res.json())
+		.catch(() => void 0);
+
+	const localVersion = semanticVersionToNumber(import.meta.env.VITE_APP_VERSION);
+	const hostedVersion = manifest ? semanticVersionToNumber(manifest.version) : 0;
+
+	if (manifest && hostedVersion > localVersion) {
 		const scr = document.createElement("script");
 		scr.id = "seventv-site-hosted";
-		scr.src = hostedURL;
+		scr.src = manifest.index_file;
 		scr.type = "module";
 
 		const style = document.createElement("link");
 		style.rel = "stylesheet";
 		style.type = "text/css";
-		style.href = `http://127.0.0.1:8080/seventv.style.3.0.3.css`;
+		style.href = manifest.stylesheet_file;
 		style.setAttribute("charset", "utf-8");
 		style.setAttribute("content", "text/html");
 		style.setAttribute("http-equiv", "content-type");
@@ -17,7 +27,10 @@
 
 		document.head.appendChild(style);
 		document.head.appendChild(scr);
+
+		log.info("<Site>", "Using Hosted Mode,", "v" + manifest.version);
 	} else {
 		import("./site.app");
+		log.info("<Site>", "Using Local Mode,", "v" + import.meta.env.VITE_APP_VERSION);
 	}
 })();
--- vite.config.hosted.ts
+++ vite.config.hosted.ts
@@ -1,6 +1,7 @@
 import { appName, getFullVersion, r } from "./vite.utils";
 import vuei18n from "@intlify/unplugin-vue-i18n/vite";
 import vue from "@vitejs/plugin-vue";
+import fs from "fs-extra";
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
 
@@ -10,13 +11,24 @@ export default defineConfig(() => {
 	const outDir = process.env.OUT_DIR || "";
 	const fullVersion = getFullVersion(isNightly);
 
+	const stylesheetFileName = `seventv.style.${fullVersion}.css`;
+	const indexFileName = `index.${fullVersion}.js`;
+
 	process.env = {
 		...process.env,
 		...loadEnv(mode, process.cwd()),
 		VITE_APP_NAME: appName,
 		VITE_APP_VERSION: fullVersion,
 		VITE_APP_VERSION_BRANCH: process.env.BRANCH,
-		VITE_APP_STYLESHEET_NAME: `seventv.style.${fullVersion}.css`,
+		VITE_APP_STYLESHEET_NAME: stylesheetFileName,
+		VITE_APP_CHANGELOG: fs.readFileSync(
+			r(
+				{
+					nightly: "CHANGELOG-nightly.md",
+				}[process.env.BRANCH ?? ""] ?? "CHANGELOG.md",
+			),
+			"utf-8",
+		),
 	};
 
 	return {
@@ -35,19 +47,22 @@ export default defineConfig(() => {
 			lib: {
 				formats: ["es"],
 				entry: r("src/site/site.app.ts"),
-				fileName: () => `index.${fullVersion}.js`,
+				fileName: () => `v${fullVersion}/index.${fullVersion}.js`,
 				name: "seventv-site",
 			},
 			rollupOptions: {
 				output: {
 					inlineDynamicImports: false,
-					manualChunks: {
-						tw: ["src/site/twitch.tv/TwitchSite.vue"],
-					},
-					assetFileNames: `seventv.[name].${fullVersion}[extname]`,
-					chunkFileNames: `seventv.[name].${fullVersion}.js`,
+					assetFileNames: `v${fullVersion}/seventv.[name].${fullVersion}[extname]`,
+					chunkFileNames: `v${fullVersion}/seventv.[name].${fullVersion}.js`,
 
 					sanitizeFileName: (name: string) => name.toLowerCase(),
+
+					manualChunks: {
+						"site.twitch.tv": ["src/site/twitch.tv/TwitchSite.vue"],
+						"site.youtube.com": ["src/site/youtube.com/YouTubeSite.vue"],
+						"site.global": ["src/site/global/Global.vue"],
+					},
 				},
 			},
 		},
@@ -57,6 +72,22 @@ export default defineConfig(() => {
 			vuei18n({
 				include: r("./locale/*"),
 			}),
+
+			// Create hosted manifest
+			{
+				name: "create-hosted-manifest",
+				async writeBundle(this) {
+					const man = {
+						version: getFullVersion(isNightly),
+						index_file: `${process.env.VITE_APP_HOST}/v${fullVersion}/${indexFileName}`,
+						stylesheet_file: `${process.env.VITE_APP_HOST}/v${fullVersion}/${stylesheetFileName}`,
+					};
+
+					setTimeout(() => {
+						fs.writeJSON(r("dist-hosted").concat("/", outDir, "/", "manifest.json"), man);
+					});
+				},
+			},
 		],
 	};
 });
