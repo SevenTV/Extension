bc86ec662f5b14bc392908d54e612bcbb47841c9
Anatole <me@anatole.dev>
2023-04-07 14:43:15 +0200

    fix: i18n

--- vite.config.hosted.ts
+++ vite.config.hosted.ts
@@ -36,6 +36,8 @@ export default defineConfig(() => {
 		resolve: {
 			alias: {
 				"@": path.resolve(__dirname, "src"),
+				"@locale": path.resolve(__dirname, "locale"),
+				"vue-i18n": "vue-i18n/dist/vue-i18n.runtime.esm-bundler.js",
 			},
 		},
 		define: {
