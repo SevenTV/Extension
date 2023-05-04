c90b22b09f9164858e3d9f0acdcef077c1c64003
Turtled <trtld2@gmail.com>
2023-05-03 17:14:44 -0700

    Add double click mention username to focus also

--- src/site/twitch.tv/modules/chat/components/message/parts/Mention.vue
+++ src/site/twitch.tv/modules/chat/components/message/parts/Mention.vue
@@ -17,6 +17,7 @@
 import type { ChatMessage, MentionToken } from "@/common/chat/ChatMessage";
 import { useChannelContext } from "@/composable/channel/useChannelContext";
 import { useChatTools } from "@/composable/chat/useChatTools";
+import { useFocusedChatters } from "@/composable/chat/useFocusedChatters";
 import { useConfig } from "@/composable/useSettings";
 import UserTag from "../UserTag.vue";
 
@@ -25,12 +26,36 @@ const props = defineProps<{
 	msg: ChatMessage;
 }>();
 
+const clickToFocusOnChatterEnabled = useConfig<boolean>("chat.click_to_focus_on_chatter");
 const shouldRenderColoredMentions = useConfig("chat.colored_mentions");
 const ctx = useChannelContext();
 const tools = useChatTools(ctx);
+const focusedChatters = useFocusedChatters();
 
+function onUsernameDoubleClick() {
+	if (!focusedChatters) return;
+	focusedChatters.value.push(props.token.content.recipient);
+	// Clear blue selection after double clicking username
+	document.getSelection()?.empty();
+}
+
+let clickTimer = -1;
 function onClick(ev: MouseEvent) {
-	tools.openViewerCard(ev, props.token.content.recipient, props.msg.id);
+	if (clickToFocusOnChatterEnabled.value) {
+		const target = ev.currentTarget;
+		if (ev.detail === 1) {
+			clickTimer = setTimeout(() => {
+				// Single click
+				tools.openViewerCard(ev, props.token.content.recipient, props.msg.id, target);
+			}, 300);
+		} else if (ev.detail >= 2) {
+			// Double click
+			clearTimeout(clickTimer);
+			onUsernameDoubleClick();
+		}
+	} else {
+		tools.openViewerCard(ev, props.token.content.recipient, props.msg.id);
+	}
 }
 </script>
 
