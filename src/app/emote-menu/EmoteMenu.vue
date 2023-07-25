68e9abf3cf8a57933757857024891448f265470e
Anatole <me@anatole.dev>
2023-07-25 12:49:07 +0200

    fix: stall due to slow third party response times

--- src/app/emote-menu/EmoteMenu.vue
+++ src/app/emote-menu/EmoteMenu.vue
@@ -225,9 +225,7 @@ onClickOutside(containerRef, (ev) => {
 				font-size: 1.5em;
 			}
 
-			transition:
-				width 120ms ease-in-out,
-				background 150ms ease-in-out;
+			transition: width 120ms ease-in-out, background 150ms ease-in-out;
 
 			&[selected="true"] {
 				background: var(--seventv-highlight-neutral-1);
