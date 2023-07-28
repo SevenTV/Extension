SevenTV/Extension

commit f7c7d6c3da0f99dcfb779746bf470171b3f4b980

fix: stall due to slow third party response times (#797)

src/worker/worker.http.ts
@@ -112,7 +112,7 @@ export class WorkerHttp {
 		] as [SevenTV.Provider, () => Promise<SevenTV.EmoteSet>][];
 
 		const onResult = async (set: SevenTV.EmoteSet) => {
-			if (!set) return;
+			if (!set || !set.id) return;
 
 			// store set to DB
 			await db.withErrorFallback(db.emoteSets.put(set), () =>
@@ -127,20 +127,17 @@ export class WorkerHttp {
 		};
 
 		// iterate results and store sets to DB
-		const sets = [] as SevenTV.EmoteSet[];
-		for (const [provider, setP] of promises) {
+		for (const [provider, fetchSetData] of promises) {
 			if (!port.providers.has(provider)) continue;
 
-			const set = await setP().catch((err) =>
-				this.driver.log.error(
-					`<Net/Http> failed to load emote set from provider ${provider} in #${channel.username}`,
-					err,
-				),
-			);
-			if (!set) continue;
-
-			sets.push(set);
-			await onResult(set);
+			fetchSetData()
+				.then(onResult)
+				.catch((err) =>
+					this.driver.log.error(
+						`<Net/Http> failed to load emote set from provider ${provider} in #${channel.username}`,
+						err,
+					),
+				);
 		}
 
 		channel.user = (await userPromise)?.user ?? undefined;
@@ -149,20 +146,6 @@ export class WorkerHttp {
 			port.postMessage("CHANNEL_FETCHED", {
 				channel,
 			});
-		}
-
-		// begin subscriptions to channel emote sets
-		for (const set of sets) {
-			if (set.provider !== "7TV") continue; // we only care about 7TV sets, as others have no events
-
-			this.driver.eventAPI.subscribe(
-				"emote_set.*",
-				{
-					object_id: set.id,
-				},
-				port,
-				channel.id,
-			);
 		}
 
 		const user = await userPromise;

commit a3ae7153a1b180995f31cceb05054258489e8b2b

Restore EventAPI updates for channel sets, and remove static cosmetic fetching
Fixes regression introduced in #797

.env.dev
@@ -2,7 +2,6 @@ NODE_ENV=development
 VITE_APP_SITE="http://localhost:4200"
 VITE_APP_API="http://localhost:3100/v3"
 VITE_APP_API_GQL="http://localhost:3000/v3/gql"
-VITE_APP_API_REST_OLD="http://localhost:3100/v2"
 VITE_APP_API_EVENTS="ws://localhost:3700/v3"
 VITE_APP_API_EGVAULT="http://localhost:3444/v1"
 VITE_APP_HOST="http://localhost:8080"

.env.production
@@ -2,7 +2,6 @@ NODE_ENV=production
 VITE_APP_SITE="https://7tv.app"
 VITE_APP_API="https://7tv.io/v3"
 VITE_APP_API_GQL="https://7tv.io/v3/gql"
-VITE_APP_API_REST_OLD="https://7tv.io/v2"
 VITE_APP_API_EVENTS="wss://events.7tv.io/v3"
 VITE_APP_API_EGVAULT="https://egvault.7tv.io/v1"
 VITE_APP_HOST="https://extension.7tv.gg"

src/worker/worker.http.ts
@@ -3,19 +3,13 @@
 import { BitField, EmoteSetFlags } from "@/common/Flags";
 import { imageHostToSrcset } from "@/common/Image";
 import { log } from "@/common/Logger";
-import {
-	convertBttvEmoteSet,
-	convertFFZEmoteSet,
-	convertFfzBadges,
-	convertSeventvOldCosmetics,
-} from "@/common/Transform";
+import { convertBttvEmoteSet, convertFFZEmoteSet, convertFfzBadges } from "@/common/Transform";
 import { db } from "@/db/idb";
 import type { WorkerDriver } from "./worker.driver";
 import type { WorkerPort } from "./worker.port";
 
 namespace API_BASE {
 	export const SEVENTV = import.meta.env.VITE_APP_API;
-	export const SEVENTV_OLD = import.meta.env.VITE_APP_API_REST_OLD;
 	export const FFZ = "https://api.frankerfacez.com/v1";
 	export const BTTV = "https://api.betterttv.net/3";
 }
@@ -124,6 +118,17 @@ export class WorkerHttp {
 				.where("id")
 				.equals(channel.id)
 				.modify((x) => x.set_ids.push(set.id));
+
+			if (set.provider == "7TV") {
+				this.driver.eventAPI.subscribe(
+					"emote_set.*",
+					{
+						object_id: set.id,
+					},
+					port,
+					channel.id,
+				);
+			}
 		};
 
 		// iterate results and store sets to DB
@@ -170,29 +175,24 @@ export class WorkerHttp {
 			this.writePresence(port.platform, port.user.id, channel.id, true);
 		}
 
-		// Send the legacy static cosmetics to the port
+		// Fetch 3rd party static cosmetics if relevant
 		Promise.all([
-			await this.API()
-				.seventv.loadOldCosmetics("twitch_id", this.driver.cache)
-				.catch((err) => log.error("Failed to load old cosmetics", err)),
 			port.providerExtensions.has("FFZ") // load ffz badges if their extension is installed
 				? await this.API()
 						.frankerfacez.loadCosmetics()
 						.catch(() => void 0)
 				: void 0,
-		]).then(([seventv, ffz]) => {
-			const converted = seventv ? convertSeventvOldCosmetics(seventv) : [];
-			const badges = [...(seventv ? converted[0] : []), ...(ffz ? convertFfzBadges(ffz) : [])];
-			const paints = converted[1] ?? [];
+		]).then(([ffz]) => {
+			const badges = [...(ffz ? convertFfzBadges(ffz) : [])];
 
 			setTimeout(() => {
 				port.postMessage("STATIC_COSMETICS_FETCHED", {
 					badges,
-					paints,
+					paints: [],
 				});
 			}, 5000);
 
-			log.info(`<Static Cosmetics> ${badges.length} badges, ${paints.length} paints`);
+			log.info(`<3rd Party Cosmetics> ${badges.length} badges`);
 		});
 	}
 
@@ -291,32 +291,6 @@ export const seventv = {
 		if (!userConn.user) return Promise.reject(new Error("No user was returned!"));
 
 		return Promise.resolve(userConn.user);
-	},
-
-	async loadOldCosmetics(
-		identifier: "twitch_id" | "login" | "object_id",
-		cache?: Cache | null,
-	): Promise<SevenTV.OldCosmeticsResponse> {
-		if (cache) {
-			const cached = await cache.match(API_BASE.SEVENTV_OLD + `/cosmetics?user_identifier=${identifier}`);
-			if (cached) {
-				log.debug("<API/Old> Old Cosmetics cache hit");
-				return Promise.resolve<SevenTV.OldCosmeticsResponse>(await cached.json());
-			}
-		}
-
-		const resp = await doRequest(API_BASE.SEVENTV_OLD, `cosmetics?user_identifier=${identifier}`).catch((err) =>
-			Promise.reject(err),
-		);
-		if (!resp || resp.status !== 200) {
-			return Promise.reject(resp);
-		}
-
-		if (cache) {
-			cache.add(resp.url);
-		}
-
-		return Promise.resolve<SevenTV.OldCosmeticsResponse>(await resp.json());
 	},
 };
 