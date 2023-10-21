4c3d9dcb41f9720dbd01289c8b8cb678157346f6
lulamae12 <tls.tommy11@gmail.com>
2023-10-20 20:55:05 -0400

    linter fixes

--- src/content/content.ts
+++ src/content/content.ts
@@ -16,8 +16,8 @@ const shouldInject = () => {
 	const inject = () => {
 		// Script
 		const script = document.createElement("script");
-		script.src = import.meta.env.DEV 
-					? import.meta.env.BASE_URL + "src/site/site.ts" : chrome.runtime.getURL("site.js");
+		script.src = import.meta.env.DEV
+				? import.meta.env.BASE_URL + "src/site/site.ts" : chrome.runtime.getURL("site.js");
 		script.id = "seventv-extension";
 		script.type = "module";
 		script.setAttribute("worker_url", chrome.runtime.getURL("worker.js"));
