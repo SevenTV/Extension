c1ebba4a518a36b8278406697e98dee33e685547
pimothyxd <29018740+pimothyxd@users.noreply.github.com>
2024-03-22 23:59:43 +0100

    chore: add Twitch icons

---
+++ src/assets/svg/twitch/TwBits.vue
@@ -0,0 +1,11 @@
+<template>
+	<svg width="20px" height="20px" version="1.1" viewBox="0 0 20 20" x="0px" y="0px" fill="var(--color-fill-current)">
+		<g>
+			<path
+				fill-rule="evenodd"
+				d="M3 12l7-10 7 10-7 6-7-6zm2.678-.338L10 5.487l4.322 6.173-.85.728L10 11l-3.473 1.39-.849-.729z"
+				clip-rule="evenodd"
+			></path>
+		</g>
+	</svg>
+</template>
