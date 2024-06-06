49d9070959357d1aa9bad45a4c0eeeb436695a99
Excellify <jarlsgu@gmail.com>
2024-06-06 23:35:21 +0200

    Use window instead of iframe

--- src/app/settings/SettingsViewProfile.vue
+++ src/app/settings/SettingsViewProfile.vue
@@ -4,10 +4,10 @@
 		You are authenticated with 7TV <br />
 		Try using the the custom 7TV commands if a channel where you have editor rights
 	</div>
-	<iframe v-if="!token" ref="iframe" :src="src" width="100%" height="100%" />
 </template>
 <script setup lang="ts">
-import { onBeforeMount, onUnmounted } from "vue";
+import { onMounted, onUnmounted } from "vue";
+import { Pausable, useIntervalFn } from "@vueuse/core";
 import { useActor } from "@/composable/useActor";
 import { useConfig } from "@/composable/useSettings";
 
@@ -15,18 +15,34 @@ const src = import.meta.env.VITE_APP_SITE + "/extension/auth";
 
 const actor = useActor();
 const token = useConfig<string>("app.7tv.token");
+
+let w: Window | null = null;
+let s: Pausable | null = null;
+
 const listener = (ev: MessageEvent) => {
-	const data = ev.data;
-	if (data && data.type === "7tv-token") {
-		token.value = data.token;
+	if (!ev.data) return;
+
+	switch (ev.data.type) {
+		case "7tv-token":
+			token.value = ev.data.token;
+			w?.close();
+			s?.pause();
+			window.removeEventListener("message", listener);
+			break;
 	}
 };
 
-onBeforeMount(() => {
+onMounted(() => {
+	w = window.open(src, "7tv-auth", "width=400,height=600");
+	if (!w) return;
 	window.addEventListener("message", listener);
+	s = useIntervalFn(() => {
+		w?.postMessage("7tv-token-request", "*");
+	}, 100);
 });
 
 onUnmounted(() => {
+	w?.close();
 	window.removeEventListener("message", listener);
 });
 </script>
