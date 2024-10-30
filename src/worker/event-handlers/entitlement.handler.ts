5da39d54a1ddb8a5ae6b1b354a28f81c44d3ea78
Excellify <jarlsgu@gmail.com>
2024-10-30 11:52:36 +0100

    Map entitlements to all connections

--- src/worker/event-handlers/entitlement.handler.ts
+++ src/worker/event-handlers/entitlement.handler.ts
@@ -10,24 +10,25 @@ export async function onEntitlementCreate(ctx: EventContext, cm: ChangeMap<Seven
 		const obj: typeof cm.object = structuredClone(cm.object);
 		if (!obj || !obj.user || !obj.user.connections?.length) return;
 
-		const cid = obj.user.connections.find((x) => x.platform === platform)?.id ?? "";
-
-		// Write to IDB
+		const ids = obj.user.connections.filter((x) => x.platform === platform).map((x) => x.id);
 		delete obj.user;
-		ctx.db.entitlements
-			.put({
-				...obj,
-				scope: port.channelIds.map((channelID) => `${platform}:${channelID ?? "X"}`).join(","),
-				user_id: cid,
-			})
-			.catch(() => ctx.db.entitlements.update(obj.id, obj));
+		ids.forEach((cid) => {
+			// Write to IDB
+			ctx.db.entitlements
+				.put({
+					...obj,
+					scope: port.channelIds.map((channelID) => `${platform}:${channelID ?? "X"}`).join(","),
+					user_id: cid,
+				})
+				.catch(() => ctx.db.entitlements.update(obj.id, obj));
 
-		// Send the entitlement to the client
-		port.postMessage("ENTITLEMENT_CREATED", {
-			id: obj.id,
-			kind: obj.kind,
-			ref_id: obj.ref_id,
-			user_id: cid,
+			// Send the entitlement to the client
+			port.postMessage("ENTITLEMENT_CREATED", {
+				id: obj.id,
+				kind: obj.kind,
+				ref_id: obj.ref_id,
+				user_id: cid,
+			});
 		});
 	}
 }
@@ -42,17 +43,20 @@ export async function onEntitlementDelete(ctx: EventContext, cm: ChangeMap<Seven
 		const obj: typeof cm.object = structuredClone(cm.object);
 		if (!obj || !obj.user || !obj.user.connections?.length) return;
 
-		const cid = obj.user.connections.find((x) => x.platform === platform)?.id ?? "";
+		obj.user.connections
+			.filter((x) => x.platform === platform)
+			.map((x) => x.id)
+			.forEach((cid) => {
+				// Write to IDB
+				ctx.db.entitlements.delete(obj.id);
 
-		// Write to IDB
-		ctx.db.entitlements.delete(obj.id);
-
-		// Send the entitlement to the client
-		port.postMessage("ENTITLEMENT_DELETED", {
-			id: obj.id,
-			kind: obj.kind,
-			ref_id: obj.ref_id,
-			user_id: cid,
-		});
+				// Send the entitlement to the client
+				port.postMessage("ENTITLEMENT_DELETED", {
+					id: obj.id,
+					kind: obj.kind,
+					ref_id: obj.ref_id,
+					user_id: cid,
+				});
+			});
 	}
 }
