7709ae57c74e13c85d6b557558739351903daf58
Excellify <jarlsgu@gmail.com>
2025-03-04 23:12:22 +0100

    disable kick auth module

--- src/types/kick.module.d.ts
+++ src/types/kick.module.d.ts
@@ -1,4 +1,4 @@
-import AuthModuleVue from "@/site/kick.com/modules/auth/AuthModule.vue";
+// import AuthModuleVue from "@/site/kick.com/modules/auth/AuthModule_disabled.vue";
 import ChatInputModuleVue from "@/site/kick.com/modules/chat-input/ChatInputModule.vue";
 import ChatModuleVue from "@/site/kick.com/modules/chat/ChatModule.vue";
 import EmoteMenuModuleVue from "@/site/kick.com/modules/emote-menu/EmoteMenuModule.vue";
@@ -7,7 +7,7 @@ import SettingsModuleVue from "@/site/kick.com/modules/settings/SettingsModule.v
 declare type KickModuleID = keyof KickModuleComponentMap;
 
 declare type KickModuleComponentMap = {
-	auth: typeof AuthModuleVue;
+	// auth: typeof AuthModuleVue;
 	chat: typeof ChatModuleVue;
 	settings: typeof SettingsModuleVue;
 	"chat-input": typeof ChatInputModuleVue;
