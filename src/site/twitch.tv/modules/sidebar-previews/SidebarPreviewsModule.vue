65679029f3ad9cd4c1181c8ae515c847c7d23c9c
jamaan-s <jamaan198@hotmail.com>
2025-06-29 17:14:17 +0300

    change variable name

--- src/site/twitch.tv/modules/sidebar-previews/SidebarPreviewsModule.vue
+++ src/site/twitch.tv/modules/sidebar-previews/SidebarPreviewsModule.vue
@@ -66,7 +66,7 @@ markAsReady();
 </script>
 
 <script lang="ts">
-const expandOnHover = useConfig<boolean>("ui.sidebar_hover_expand");
+const isSidebarHoverExpand = useConfig<boolean>("ui.sidebar_hover_expand");
 export const config = [
 	declareConfig("ui.sidebar_previews", "TOGGLE", {
 		path: ["Appearance", "Interface"],
@@ -92,7 +92,7 @@ export const config = [
 			unit: "ms",
 		},
 		defaultValue: 0,
-		disabledIf: () => !expandOnHover.value
+		disabledIf: () => !isSidebarHoverExpand.value
 	}),
 ];
 </script>
