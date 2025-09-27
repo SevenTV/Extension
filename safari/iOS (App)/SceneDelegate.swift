33cfecc795eeabb3ca0a283524dfd1d3ffc8be92
LilSylent <czimbercsik.bence2003@gmail.com>
2025-09-27 15:21:44 +0200

    added Safari

---
+++ safari/iOS (App)/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  iOS (App)
+//
+//  Created by Czimbercsik Bence on 2025. 09. 27..
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
