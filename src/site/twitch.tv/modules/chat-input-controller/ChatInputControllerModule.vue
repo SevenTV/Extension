cb81bb29936ab5806e341a9ceea987433f163e27
Rico <rico@7tv.app>
2026-07-01 13:30:30 +0200

    fix: do not hide mod view + shield mode

--- src/site/twitch.tv/modules/chat-input-controller/ChatInputControllerModule.vue
+++ src/site/twitch.tv/modules/chat-input-controller/ChatInputControllerModule.vue
@@ -145,9 +145,5 @@ div[data-test-selector="chat-input-buttons-container"] seventv-chat-input-button
 
 div[data-test-selector="chat-input-buttons-container"] > div:has(button[data-a-target="chat-settings"]) {
 	display: contents !important;
-
-	> :not(:has(button[data-a-target="chat-settings"])) {
-		display: none !important;
-	}
 }
 </style>
